6a93e2e2bfb1d407e009f5ff3b457921a0db5f3c
Linh Pam <benpam099@gmail.com>
2022-03-31 14:01:02 +0700

    add code

--- code.py
+++ code.py
@@ -1,1 +1,24 @@
-import pygame
+import pygame, sys
+def draw_floor():
+    screen.blit(floor,(floor_x_pos,600))
+    screen.blit(floor,(floor_x_pos+432,600))
+pygame.init()
+screen= pygame.display.set_mode((432,768))
+clock = pygame.time.Clock()
+bg = pygame.image.load('FileGame/assets/background-night.png')
+bg = pygame.transform.scale2x(bg)
+floor = pygame.image.load('FileGame/assets/floor.png')
+floor = pygame.transform.scale2x(floor)
+floor_x_pos = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.blit(bg,(0,0))      
+    floor_x_pos -=1  
+    draw_floor()
+    if floor_x_pos <=432:
+        floor_x_pos =0
+    pygame.display.update()
+    clock.tick(120)
